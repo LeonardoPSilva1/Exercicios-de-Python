bbea12c76f6d2eb86c770ce30873548135fa9a17
Leonardo Pereira da Silva <leonardop.silva1622@gmail.com>
2023-01-30 04:01:14 -0300

    Lições de python

---
+++ Desafio 14.py
@@ -0,0 +1,9 @@
+#teste
+
+
+salario = float(input('Digite seu salario aqui: '))
+aumento = salario * 15 / 100
+final = salario + aumento
+
+print('aumento: {}R$'.format(aumento))
+print('salario final de: {}'.format(final))
