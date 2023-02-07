79c0a53464d3530ae0de28aa87cc750db0f7086f
Leonardo Pereira da Silva <leonardop.silva1622@gmail.com>
2023-02-06 21:47:09 -0300

    Lições de python desafio

---
+++ Desafio 27.py
@@ -0,0 +1,8 @@
+n = str(input('Digite seu nome completo: ')).strip()
+nome = n.split()
+print('Seu primeiro nome: {}'.format(nome[0]))
+print('Seu nultimo nome é: {}'.format(nome[len(nome) - 1]))
+
+
+
+
