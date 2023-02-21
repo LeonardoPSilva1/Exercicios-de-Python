920d8c38458d3c5fb8738b9deef265e0bb3560a6
Leonardo Pereira da Silva <leonardop.silva1622@gmail.com>
2023-02-21 13:23:12 -0300

    Lições de python desafio

--- Desafio 36.py
+++ Desafio 36.py
@@ -13,8 +13,8 @@ minimo = salario * 30 / 100
 print('Calculando...')
 sleep(5)
 
-print('Para pagar a casa de {:.3f} em {:.0f} anos'.format(Casa, anos), end='')
-print(' a prestação da casa sera de {}'.format(prestacao))
+print('Para pagar a casa de {:.0f} em {:.0f} anos'.format(Casa, anos), end='')
+print(' a prestação da casa sera de {:.3f}'.format(prestacao))
 
 if prestacao <= minimo:
     print('Tudo ok, Emprestimo concedido')
